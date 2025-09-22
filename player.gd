4a3bc4ebd76c15165a0ac680b28f9df9fbfdf05d
BoaDuvet <margoshnya7@gmail.com>
2025-09-22 21:45:05 +0500

    почти

--- player.gd
+++ player.gd
@@ -35,11 +35,11 @@ func _process(delta):
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D):
 	hide() 
 	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
+	
 func start(pos):
 	position = pos
 	show()
