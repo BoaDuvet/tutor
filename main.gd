0bed0eca4f5524c87bc77bde407a56bd15bfac0c
BoaDuvet <margoshnya7@gmail.com>
2025-09-22 17:38:36 +0500

    f

--- main.gd
+++ main.gd
@@ -10,3 +10,18 @@ func game_over():
 func new_game():
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+
+func _on_score_timer_timeout():
+	score += 1
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
